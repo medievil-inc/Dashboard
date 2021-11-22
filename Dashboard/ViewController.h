69ed9ab8cc5bf25216fbccfca19902a536cf2ea3
Иван Кудинов <medievil.inc@gmail.com>
2021-11-22 15:56:28 +0600

    Initial commit

---
+++ Dashboard/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Dashboard
+//
+//  Created by Ivan Kudinov on 22/11/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Dashboard/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Dashboard
+//
+//  Created by Ivan Kudinov on 22/11/21.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
